ea6fd406ae88b4348883fe1ee72089556c19162c
Ryan Kepler <ryankepler@Judes-MBP.fios-router.home>
2018-05-24 15:29:22 -0400

    update with differential taxa

--- PHP_workflow_loop.r
+++ PHP_workflow_loop.r
@@ -33,13 +33,22 @@ options(warnings=-1)
 # Import output from dada2 and metadata ####
 # create phyloseq object and change taxa names to something shorter
 
-phpmeta<-readRDS("phpmeta_corrected.rds")
-phpmeta$year<-as.factor(phpmeta$year)
+
 seqtab.new<-readRDS("fungi_seqs2.rds")
 phpfuntax.new<-readRDS(file = "fungi_taxa2.rds")
 phpfuntax.new <- cbind( phpfuntax.new, "seq" = rownames(phpfuntax.new), "fasta_name" = paste0("phpfung", seq(1:nrow(phpfuntax.new))))
+phpmeta<-readRDS("phpmeta_corrected.rds")
+# Fix Stoneville System.loc names
+phpmeta$System.loc <- as.character(phpmeta$System.loc)
+phpmeta$Herbicide_History <- as.character(phpmeta$Herbicide_History)
+phpmeta$System.loc[phpmeta$Location == "Stoneville"] <- paste(phpmeta$System.loc[phpmeta$Location == "Stoneville"], phpmeta$Herbicide_History[phpmeta$Location == "Stoneville"], sep = "_")
+phpmeta$System.loc <- as.factor(phpmeta$System.loc)
+phpmeta$Herbicide_History <- as.factor(phpmeta$Herbicide_History)
+phpmeta$year<-as.factor(phpmeta$year)
+# Make alternate sampleID and variables
 phpmeta$group <- factor(paste(phpmeta$System.loc, phpmeta$Glyphosphate_Treatment, sep = "_"))
 phpmeta$q_id <- factor(paste(phpmeta$System.loc, phpmeta$Glyphosphate_Treatment, phpmeta$Soil_Zone, phpmeta$year, sep = "_"))
+
 
 ps.new <- phyloseq(otu_table(seqtab.new, taxa_are_rows=FALSE), 
                    sample_data(phpmeta), 
@@ -54,6 +63,10 @@ if (identical(taxa_names(ps.new), as.character(phpfuntax.new[,"seq"]))) {
 
 taxa_names(ps.new)[1:5]
 
+phpmeta$System.loc[phpmeta$Location == "Stoneville"] <- paste(phpmeta$System.loc[phpmeta$Location == "Stoneville"], phpmeta$Herbicide_History[phpmeta$Location == "Stoneville"], sep = "_")
+
+taxa_names(ps.new)[1:5]
+
 ntaxa(ps.new)
 ps.new <- subset_taxa(ps.new, Kingdom == "k__Fungi")
 ntaxa(ps.new)
@@ -63,21 +76,19 @@ ntaxa(ps.new)
 # this version is for Location, Soil_Zone, crop 
 # Genotype is hardcoded for this version. See line BLAH
 
-unique.meta <- as.data.frame(unique(phpmeta[c("Location", "Soil_Zone", "crop")]))
-
-unique.meta <- unique.meta[(unique.meta$Location == "Beltsville"),]
+unique.meta <- as.data.frame(unique(phpmeta[c("Location", "crop")]))
+unique.meta <- unique.meta[(unique.meta$Location == "Stoneville"),]
 
 registerDoMC(cores = 8)
 
-#ptm <- proc.time()
+ptm <- proc.time()
 foreach(a=1:nrow(unique.meta), .packages = c("phyloseq")) %dopar% { 
   # get variable values
   loc <- as.character(unique.meta["Location"][a,])
-  soil <- as.character(unique.meta["Soil_Zone"][a,])
   crop <- as.character(unique.meta["crop"][a,])
   
   # make a name to recycle
-  thing <- paste(loc, soil, crop, sep = "_")
+  thing <- paste(loc, crop, sep = "_")
   
   # make folder tree to save output
   out.dir <- paste(getwd(),loc,thing, sep = "/")
@@ -87,9 +98,8 @@ foreach(a=1:nrow(unique.meta), .packages = c("phyloseq")) %dopar% {
   #loc.list <- as.character(get_variable(ps.new, "Location")) == loc
   loc.row <- row.names(sample_data(ps.new)[sample_data(ps.new)$Location %in% loc, ])
   crop.row <- row.names(sample_data(ps.new)[sample_data(ps.new)$crop %in% crop, ])
-  soil.row <- row.names(sample_data(ps.new)[sample_data(ps.new)$Soil_Zone %in% soil, ])
   genotype.row <- row.names(phpmeta[phpmeta$genotype %in% "RR", ])
-  common.row <- Reduce(intersect, list(loc.row, crop.row, soil.row, genotype.row))
+  common.row <- Reduce(intersect, list(loc.row, crop.row, genotype.row))
   
   # prune phyloseq object to desired samples
   ps.0 <- prune_samples(common.row, ps.new)
@@ -97,7 +107,7 @@ foreach(a=1:nrow(unique.meta), .packages = c("phyloseq")) %dopar% {
   saveRDS(ps.0, file.path(out.dir, paste(thing,".rds", sep = "")))
   
   # DESeq2 variance stablization
-  ps.ds <- phyloseq_to_deseq2(ps.0, ~ group + Sampling_date + group:Sampling_date)
+  ps.ds <- phyloseq_to_deseq2(ps.0, ~ group + year + Sampling_date + group:Sampling_date)
   ps.ds = estimateSizeFactors(ps.ds)
   ps.ds = estimateDispersions(ps.ds)
   psVST = getVarianceStabilizedData(ps.ds)
@@ -118,131 +128,108 @@ foreach(a=1:nrow(unique.meta), .packages = c("phyloseq")) %dopar% {
   write.table(qiime, sep = "\t", file = file.path(out.dir, paste(thing,"txt", sep = ".")), row.names = F, quote = F)
   
   # perform PERMANOVA 
-  out <- adonis(t(otu_table(ps.0)) ~ System.loc * Glyphosphate_Treatment * Sampling_date, strata = sample_data(ps.0)$Loc_plot_ID, as(sample_data(ps.0), "data.frame"))
-  write.table(as.matrix(out$aov.tab), file = file.path(out.dir, paste(thing,"perm", "txt", sep = "."))  , quote = F, sep = ",")
+  out <- adonis(t(otu_table(ps.0)) ~ System.loc * year * Soil_Zone * Glyphosphate_Treatment * Sampling_date, strata = sample_data(ps.0)$Loc_plot_ID, as(sample_data(ps.0), "data.frame"))
+  
+  write.table(as.matrix(cbind(rownames(out$aov.tab), out$aov.tab)), 
+              file = file.path(out.dir, paste(thing,"perm", "txt", sep = ".")), 
+              row.names = F, 
+              col.names = c("Variable", colnames(out$aov.tab)),
+              quote = F, sep = "\t")
 }
-#proc.time() - ptm
+proc.time() - ptm
 
-# heatmap for some of the saved subsets ####
+# Unix and Perl one-liner to combine all PERMANOVA output. 
+# Execute from top level directory for each site. Edit as necessary.
+# tail -n +1 Stoneville_*/*.perm.txt | perl -p -e 's/^.*\/(\w+)\.perm\.txt.*$/$1/g' > all_permanova.txt
 
-sv.soy.rhiz <- readRDS("Stoneville/Stoneville_rhizosphere_soy/Stoneville_rhizosphere_soy.rds")
-sv.soy.rhiz.VST <- readRDS("Stoneville/Stoneville_rhizosphere_soy/Stoneville_rhizosphere_soy_vst.rds")
+# Test some of the saved subsets ####
 
-sv.soy.rhiz.VST[sv.soy.rhiz.VST < 0.0] <- 0.0
-otu_table(sv.soy.rhiz) <- otu_table(sv.soy.rhiz.VST, taxa_are_rows = TRUE)
-out <- adonis(t(otu_table(sv.soy.rhiz)) ~ System.loc * Glyphosphate_Treatment * Sampling_date, strata = sample_data(sv.soy.rhiz)$Loc_plot_ID, as(sample_data(sv.soy.rhiz), "data.frame"))
+sv.soy <- readRDS("Stoneville/Stoneville_soy/Stoneville_soy.rds")
+sv.soy.VST <- readRDS("Stoneville/Stoneville_soy/Stoneville_soy_vst.rds")
 
+sv.soy.VST[sv.soy.VST < 0.0] <- 0.0
+otu_table(sv.soy) <- otu_table(sv.soy.VST, taxa_are_rows = TRUE)
 
-# homogeneity of variances ####
-fsr.dist <- dist(otu_table(fsp.soy.rhiz), method = "euclidean")
-fsr.beta <- betadisper(fsr.dist, sample_data(fsp.soy.rhiz)$Sampling_date)
-anova(fsr.beta)
-fsr.beta
- 
+# taxon abundance across samples ####
 
-gpt <- prune_taxa(names(sort(taxa_sums(fsp.soy.rhiz),TRUE)[1:300]), fsp.soy.rhiz)
-plot_heatmap(gpt, sample.label="group")
+sv.soy.DS <-phyloseq_to_deseq2(sv.soy, ~ group + year + Sampling_date + group:Sampling_date)
 
-# differential expression ####
+#diagdds = DESeq(fsp.soy.rhizDS, test="Wald", fitType="parametric")
 
-fsp.soy.rhizDS <-phyloseq_to_deseq2(fsp.soy.rhiz, ~ group + Sampling_date + group:Sampling_date)
+#vsd <- vst(diagdds)
+select <- order(rowMeans(counts(sv.soy.DS,normalized=F)),
+                decreasing=TRUE)[1:20]
+df <- as.data.frame(colData(sv.soy.DS)[,c("group","Sampling_date")])
 
-diagdds = DESeq(fsp.soy.rhizDS, test="Wald", fitType="parametric")
+df <- df[with(df, order(group,Sampling_date)), ]
+vsdASSAY <- sv.soy.VST[select,]
+
+vsdASSAY <- vsdASSAY[,rownames(df)]
+
+heat.tax <- as.data.frame(tax_table(sv.soy)[row.names(sv.soy.VST)])
+heat.tax <- lapply(heat.tax, function(x) gsub("^[a-z]_\\w", "", x, perl = T))
+
+row.names(sv.soy.VST) <- paste(heat.tax$fasta_name, heat.tax$Genus, heat.tax$Species, sep = "_")
+
+#assay(vsd)[select,]
+pheatmap(vsdASSAY, cluster_rows=FALSE, show_rownames=T,
+         cluster_cols=F, annotation_col=df) #,
+         #filename = "sv_soy_heat.pdf")
+
+# fsp heat maps ####
+fsp.corn <- readRDS("Beltsville/Beltsville_corn/Beltsville_corn.rds")
+
+#fsp.corn.VST <- readRDS("Beltsville/Beltsville_corn/Beltsville_corn_vst.rds")
+
+#fsp.corn.VST[fsp.corn.VST < 0.0] <- 0.0
+#otu_table(fsp.corn) <- otu_table(fsp.corn.VST, taxa_are_rows = TRUE)
+
+# taxon abundance across samples ####
+sample_data(fsp.corn)$group <- factor(paste0(sample_data(fsp.corn)$group, sample_data(fsp.corn)$Sampling_date))
+fsp.corn.DS <-phyloseq_to_deseq2(fsp.corn, ~ group)
+diagdds = DESeq(fsp.corn.DS, test="Wald", fitType="parametric")
 
 vsd <- vst(diagdds)
 select <- order(rowMeans(counts(diagdds,normalized=TRUE)),
                 decreasing=TRUE)[1:20]
+select <- taxa_names(subset_taxa(fsp.corn, Order == "o__Hypocreales"))
 df <- as.data.frame(colData(diagdds)[,c("group","Sampling_date")])
 
 df <- df[with(df, order(group,Sampling_date)), ]
-vsdASSAY <- assay(vsd)[select,]
+vsdASSAY <-assay(vsd)[select,]
 
 vsdASSAY <- vsdASSAY[,rownames(df)]
 
-heat.tax <- as.data.frame(tax_table(fsp.soy.rhiz)[row.names(vsdASSAY)])
+heat.tax <- as.data.frame(tax_table(fsp.corn)[row.names(vsdASSAY)])
 heat.tax <- lapply(heat.tax, function(x) gsub("^[a-z]_\\w", "", x, perl = T))
 
-row.names(vsdASSAY) <- paste(heat.tax$fasta_name, heat.tax$Genus, heat.tax$Species, sep = "_")
-
+row.names(vsdASSAY) <- paste(heat.tax$Genus, heat.tax$Species, heat.tax$fasta_name, sep = "_")
+vsdASSAY <- vsdASSAY[order(row.names(vsdASSAY)), ]
 #assay(vsd)[select,]
-pheatmap(vsdASSAY, cluster_rows=FALSE, show_rownames=T,
-         cluster_cols=F, annotation_col=df)
+pheatmap(vsdASSAY, cluster_rows=FALSE, show_rownames=T, 
+         show_colnames = F,
+         cluster_cols=F, annotation_col=df) #,
+         #filename = "fsp_corn_vst_nectriaceae_heat.pdf")
+# Differentially abundant taxa 
+res <- results(diagdds, contrast = c("group", "CT_spraypost", "CT_no_spraypost"))
+alpha = 0.01
+sigtab = res[which(res$padj < alpha), ]
+sigtab = cbind(as(sigtab, "data.frame"), as(tax_table(fsp.corn)[rownames(sigtab), ], "matrix"))
+#head(sigtab)
 
+theme_set(theme_bw())
+scale_fill_discrete <- function(palname = "Set1", ...) {
+  scale_fill_brewer(palette = palname, ...)
+}
 # Phylum order
-x = tapply(sigtab$log2FoldChange, sigtab$Phylum, function(x) max(x))
+x = tapply(sigtab$log2FoldChange, sigtab$Order, function(x) max(x))
 x = sort(x, TRUE)
-sigtab$Phylum = factor(as.character(sigtab$Phylum), levels=names(x))
+sigtab$Order = factor(as.character(sigtab$Order), levels=names(x))
 # Genus order
 x = tapply(sigtab$log2FoldChange, sigtab$Genus, function(x) max(x))
 x = sort(x, TRUE)
 sigtab$Genus = factor(as.character(sigtab$Genus), levels=names(x))
-ggplot(sigtab, aes(x=Genus, y=log2FoldChange, color=Phylum)) + geom_point(size=6) + 
-  theme(axis.text.x = element_text(angle = -90, hjust = 0, vjust=0.5))
-#####
-fsp.corn.rhiz <- readRDS("Beltsville/Beltsville_rhizosphere_corn/Beltsville_rhizosphere_corn.rds")
-fsp.corn.rhizDS <-phyloseq_to_deseq2(fsp.corn.rhiz, ~ group + Sampling_date + group:Sampling_date)
-
-diagdds = DESeq(fsp.corn.rhizDS, test="Wald", fitType="parametric")
-
-vsd <- vst(diagdds)
-select <- order(rowMeans(counts(diagdds,normalized=TRUE)),
-                decreasing=TRUE)[1:20]
-df <- as.data.frame(colData(diagdds)[,c("group","Sampling_date")])
-
-df <- df[with(df, order(group,Sampling_date)), ]
-vsdASSAY <- assay(vsd)[select,]
-
-vsdASSAY <- vsdASSAY[,rownames(df)]
-
-heat.tax <- as.data.frame(tax_table(fsp.corn.rhiz)[row.names(vsdASSAY)])
-heat.tax <- lapply(heat.tax, function(x) gsub("^[a-z]_\\w", "", x, perl = T))
-
-row.names(vsdASSAY) <- paste(heat.tax$fasta_name, heat.tax$Genus, heat.tax$Species, sep = "_")
-
-#assay(vsd)[select,]
-pheatmap(vsdASSAY, cluster_rows=FALSE, show_rownames=T,
-         cluster_cols=F, annotation_col=df)
-
-
+ggplot(sigtab, aes(x=log2FoldChange, y=Genus, color=Order)) + geom_point(size=6) + 
+  theme(axis.text.x = element_text(angle = -90, hjust = 0, vjust=0.5)) +
+  ggtitle("CT_spraypost CT_no_spraypost")
 # END ####
-
-fsp.RRsoy <- subset_samples(ps.new, Location == "Beltsville" & genotype == "RR" & crop == "corn" & Soil_Zone =="bulk") #  & Sampling_date == "pre" & crop == "soy"  & Glyphosphate_Treatment == "no_spray")
-fsp.RRsoy <- prune_taxa(taxa_sums(fsp.RRsoy) > 10, fsp.RRsoy)
-ntaxa(fsp.RRsoy)
-nsamples(fsp.RRsoy)
-
-# DESeq routines
-# make DESeq object
-fspDS <- phyloseq_to_deseq2(fsp.RRsoy, ~ Glyphosphate_Treatment)
-
-# Set the model to something meaningful
-design(fspDS) <- formula(~ group + Sampling_date + group:Sampling_date)
-
-# Perform variance stabilization
-# Start the clock
-ptm <- proc.time()
-fspDS = estimateSizeFactors(fspDS)
-fspDS = estimateDispersions(fspDS)
-fspVST = getVarianceStabilizedData(fspDS)
-# Stop the clock
-proc.time() - ptm
-
-# use VST counts
-# backup original FSP-Soy-RR phyloseq object
-fsp.RRsoy0 <- fsp.RRsoy
-fspVST0 <- fspVST
-fspVST[fspVST < 0.0] <- 0.0
-
-otu_table(fsp.RRsoy) <- otu_table(fspVST, taxa_are_rows = TRUE)
-ntaxa(fsp.RRsoy)
-fsp.ord.pca.bray <- ordinate(fsp.RRsoy, method="PCoA", distance="bray")
-fsp.RRsoy.eigen <- eigen(fsp.ord.pca.bray)
-
-fsp.plot<-plot_ordination(fsp.RRsoy, fsp.ord.pca.bray, type="samples", color="System.loc", shape="Glyphosphate_Treatment") 
-fsp.plot + ggtitle("FSP-Soy") +
-  geom_point(size = 3)
-
-fsp.RRsoy.axisvals <- fsp.ord.pca.bray$vectors
-
-fsp.soy.qiime <- merge(as.data.frame(sample_data(fsp.RRsoy)), fsp.RRsoy.axisvals, by = "row.names")
-write.table(fsp.soy.qiime, sep = "\t", file = "fsp.soy.qiime.txt", row.names = F, quote = F)
